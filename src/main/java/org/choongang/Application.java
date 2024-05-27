3ff3b1497a74f5f52f1b255f94c202c28dac134a
siwon9 <siwon10823@gmail.com>
2024-05-27 12:24:26 +0900

    5월27일 수업내용

--- src/main/java/org/choongang/Application.java
+++ src/main/java/org/choongang/Application.java
@@ -1,7 +1,9 @@
 package org.choongang;
 
+import org.choongang.main.MainRouter;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("program start!");
+        MainRouter.getInstance().start();
     }
 }
---
+++ src/main/java/org/choongang/main/MainRouter.java
@@ -0,0 +1,41 @@
+package org.choongang.main;
+
+import org.choongang.global.Controller;
+import org.choongang.global.Router;
+import org.choongang.global.constants.Menu;
+import org.choongang.main.controllers.MainController;
+import org.choongang.member.controllers.JoinController;
+import org.choongang.member.controllers.LoginController;
+
+public class MainRouter implements Router {
+
+    private static Router instance;
+
+    private MainRouter() {}
+
+    public static Router getInstance() {
+        if(instance == null) {
+            instance = new MainRouter();
+        }
+        return instance;
+    }
+
+    @Override
+    public void change(Menu menu) {
+        Controller controller = null;
+        switch (menu) {
+            case JOIN: controller = new JoinController(); break;
+            case LOGIN : controller = new LoginController(); break;
+            default : controller = new MainController();
+        }
+        controller.run(); // common(), show(), prompt()가 연달아 실행
+    }
+
+    @Override
+    public void start() {
+        while(true) {
+            change(Menu.MAIN); // 첫 화면은 메인 컨트롤러 출력화면
+
+        }
+    }
+}
