213cd581bb58ebc288868f221977bb51ccdd80d3
siwon9 <siwon10823@gmail.com>
2024-05-27 11:01:19 +0900

    5월27일 수업내용

---
+++ src/main/java/org/choongang/global/constants/Menu.java
@@ -0,0 +1,8 @@
+package org.choongang.global.constants;
+// 도메인에서 쓸거는 도메인에 입력하고 여기에는 공통된 상수만 입력하면된다.
+public enum Menu {
+    MAIN, // main screen
+    JOIN, // Create new account
+    LOGIN,
+    MYPAGE,
+}
